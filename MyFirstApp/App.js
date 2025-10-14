05a795a6670aa02be3c8f65d57cfe8833d056fdb
Ravi Kishan <kishanravi887321@gmail.com>
2025-10-14 16:57:06 +0530

    Update App.js text content for clarity and personalization

--- MyFirstApp/App.js
+++ MyFirstApp/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! okaty i can undertstand that its preety cool the 
+        debugging 
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
